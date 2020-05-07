8fa609f90dea87dccfb50b34cb01bb877e65bf93
Matheus <matheus.cordeiro31@gmail.com>
2020-05-07 14:39:15 -0300

    Adcionado Department

---
+++ Sales/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sales.Models;
+
+namespace Sales.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+
+            
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department(1, "Eletronics"));
+            list.Add(new Department(2, "Fashion"));
+
+            return View(list);
+        }
+    }
+}
