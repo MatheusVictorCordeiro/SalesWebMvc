4e373ad3332d8a53de3d2f2f9fc8077b0e012fed
Matheus <matheus.cordeiro31@gmail.com>
2020-05-07 16:19:06 -0300

    Salvando no banco de dados

--- Sales/Models/Department.cs
+++ Sales/Models/Department.cs
@@ -10,11 +10,7 @@ namespace Sales.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public Department(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+       
     }
 }
 
