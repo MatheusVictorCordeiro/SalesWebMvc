58f1733c61a24d9c7d8d22c31c3656888819050f
Matheus <matheus.cordeiro31@gmail.com>
2020-05-08 16:02:34 -0300

    Seller service and basic find all

---
+++ Sales/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using Sales.Data;
+using Sales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sales.Services
+{
+    public class SellerService
+    {
+        private readonly SalesContext _context;
+
+        public SellerService(SalesContext context)
+        {
+
+            _context = context;
+        }
+
+
+        public List<Seller> FindAll()
+        {
+
+            return _context.Seller.ToList();
+        }
+    }
+}
