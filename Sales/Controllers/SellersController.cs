a674e07def27d9b2db111410d650021b979288c2
Matheus <matheus.cordeiro31@gmail.com>
2020-05-13 16:28:14 -0300

    Delete seller

--- Sales/Controllers/SellersController.cs
+++ Sales/Controllers/SellersController.cs
@@ -49,6 +49,29 @@ namespace Sales.Controllers
             return RedirectToAction(nameof(Index));
 
         }
+        //esse metodo Delete não deleta, vai pra tela de confirmação do Delete.
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
     }
 }
