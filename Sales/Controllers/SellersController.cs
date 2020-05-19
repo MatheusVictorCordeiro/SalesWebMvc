be5e47b6d60f7c864faa2878b800fa7ae154c46a
Matheus <matheus.cordeiro31@gmail.com>
2020-05-19 15:35:53 -0300

    Updtate Seller

--- Sales/Controllers/SellersController.cs
+++ Sales/Controllers/SellersController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Sales.Models;
 using Sales.Models.ViewModels;
 
 using Sales.Services;
+using Sales.Services.Exceptions;
 
 namespace Sales.Controllers
 {
@@ -84,6 +86,50 @@ namespace Sales.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+
+                return BadRequest();
+            }
+
+
+
+        }
+
 
     }
 }
--- Sales/Services/SellerService.cs
+++ Sales/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Sales.Services.Exceptions;
 
 namespace Sales.Services
 {
@@ -44,6 +45,24 @@ namespace Sales.Services
             _context.SaveChanges();
 
 
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id)){
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+
+            }
+            catch(DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+           
         }
     }
 }
