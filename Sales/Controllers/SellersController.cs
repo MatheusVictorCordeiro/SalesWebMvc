c711b15e384ba99788314978275c0a9d8d5bd0ae
Matheus <matheus.cordeiro31@gmail.com>
2020-05-20 14:51:17 -0300

    Validation

--- Sales/Controllers/SellersController.cs
+++ Sales/Controllers/SellersController.cs
@@ -49,6 +49,14 @@ namespace Sales.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            //testa se o Modelo foi validado
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
 
@@ -93,6 +101,8 @@ namespace Sales.Controllers
         {
             if (id == null)
             {
+                
+                
                 return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
@@ -110,7 +120,18 @@ namespace Sales.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if (id != seller.Id)
+            
+                //testa se o Modelo foi validado
+                if (!ModelState.IsValid)
+                {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+                return View(viewModel);
+                }
+
+
+                if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
--- Sales/Models/Seller.cs
+++ Sales/Models/Seller.cs
@@ -10,17 +10,36 @@ namespace Sales.Models
     {
 
         public int Id { get; set; }
+
+
+        [Required (ErrorMessage ="{0} is required")]
+        [StringLength(60,MinimumLength =3,ErrorMessage ="{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "{0} is required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+
         public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "{0} is required")]
+        
         [Display(Name="Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(100.0,50000.0,ErrorMessage ="{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]//duas casas decimais.
         public double BaseSalary { get; set; }
 
+
+     
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SallesRecord> Sales { get; set; } = new List<SallesRecord>();
