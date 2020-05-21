90c124d2eddd58b2dfebf41f4004fea95c966b80
Matheus <matheus.cordeiro31@gmail.com>
2020-05-21 16:39:23 -0300

    Impementing grouping search

--- Sales/Services/SallesRecordService.cs
+++ Sales/Services/SallesRecordService.cs
@@ -42,5 +42,30 @@ namespace Sales.Services
                 .ToListAsync();
 
         }
+
+        public async Task<List<IGrouping<Department,SallesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            //construindo um objeto Iqueryable, podemos construir consultas em cima dele.
+            var result = from obj in _context.SallesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+
+                //fazendo Join em outras tabelas.
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x=> x.Seller.Department)
+                .ToListAsync();
+
+        }
     }
 }
