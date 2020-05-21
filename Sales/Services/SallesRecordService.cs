1e57c29265acdb8e07a9f6a71f06f9211e8d65e5
Matheus <matheus.cordeiro31@gmail.com>
2020-05-21 15:35:30 -0300

    Impementing simple search

---
+++ Sales/Services/SallesRecordService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Data;
+using Sales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sales.Services
+{
+    public class SallesRecordService
+    {
+
+        private readonly SalesContext _context;
+
+        public SallesRecordService(SalesContext context)
+        {
+
+            _context = context;
+        }
+
+        public async Task<List<SallesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            //construindo um objeto Iqueryable, podemos construir consultas em cima dele.
+            var result = from obj in _context.SallesRecord select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+
+                //fazendo Join em outras tabelas.
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+
+        }
+    }
+}
